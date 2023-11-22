7ad110122408b4b2c9ffeec406fc866f1cc079b8
To'xtasin Jo'rayev <olmalar1999@gmail.com>
2023-11-22 16:32:53 +0500

    22.11.2023 db local

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=andalofa_adu3',
-    'username' => 'andalofa_user',
-    'password' => 'aa123456',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
